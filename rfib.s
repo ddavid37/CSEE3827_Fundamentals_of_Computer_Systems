6c50dac0dcb283e79a752220c664ed426ccdc74b
Daniel David <ddavid37@montgomerycollege.edu>
2024-11-05 12:54:40 -0500

    writing the functions for the Fibonacci function in assembly

--- rfib.s
+++ rfib.s
@@ -1,7 +1,35 @@
-rfib:
-    # Replace these two instructions with your function
-    li      $v0,            0
-    jr      $ra
+    addi    $sp,            $sp,    -8              # Allocate stack space
+    sw      $ra,            4($sp)                  # Save return address
+    sw      $a0,            0($sp)                  # Save argument
+
+    li      $t0,            1                       # Base case: if n == 1
+    beq     $a0,            $t0,    base_case_one
+    li      $t0,            0                       # Base case: if n == 0
+    beq     $a0,            $t0,    base_case_zero
+
+    addi    $a0,            $a0,    -1              # Recursive case: rfib(n-1)
+    jal     rfib
+    move    $t1,            $v0                     # Save rfib(n-1) in $t1
+
+    lw      $a0,            0($sp)                  # Restore argument
+    addi    $a0,            $a0,    -2              # Recursive case: rfib(n-2)
+    jal     rfib
+    add     $v0,            $v0,    $t1             # rfib(n) = rfib(n-1) + rfib(n-2)
+    j       end_rfib
+
+base_case_one:
+    li      $v0,            1                       # rfib(1) = 1
+    j       end_rfib
+
+base_case_zero:
+    li      $v0,            0                       # rfib(0) = 0
+
+end_rfib:
+    lw      $ra,            4($sp)                  # Restore return address
+    lw      $a0,            0($sp)                  # Restore argument
+    addi    $sp,            $sp,    8               # Deallocate stack space
+    jr      $ra                                     # Return
+
     #### Do not remove this separator. Place all of your code above this line. ####
 main:
     addi    $sp,            $sp,    -4
